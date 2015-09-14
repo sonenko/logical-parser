3a2a1f2fed5e3e9a9c4e5de60f6c1f0db1fb2c61
sonenko <growler.ua@gmail.com>
2015-09-14 18:19:10 +0300

    Attempt to release

--- project/project/ProjectBuild.scala
+++ project/project/ProjectBuild.scala
@@ -7,7 +7,6 @@ object ProjectBuild extends Build {
   lazy val projectBuild = project.in(file(".")).settings(List(
     addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3"),
     addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.3"),
-    addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0"),
-    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+    addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
   ): _*)
 }
