be433a232efa2e1c11e2e181aa5ad8c17171c46e
sonenko <growler.ua@gmail.com>
2015-09-18 16:28:02 +0300

    remove idea plugin

--- project/project/ProjectBuild.scala
+++ project/project/ProjectBuild.scala
@@ -6,7 +6,6 @@ import sbt.Keys._
 object ProjectBuild extends Build {
   lazy val projectBuild = project.in(file(".")).settings(List(
     addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3"),
-    addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.3"),
-    addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
+    addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.3")
   ): _*)
 }
