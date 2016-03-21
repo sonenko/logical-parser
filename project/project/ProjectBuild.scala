cd87a2ba255c0a7542b5660e1794674ea4721f1e
sonenko <growler.ua@gmail.com>
2016-03-22 01:08:56 +0200

    update sbt release

--- project/project/ProjectBuild.scala
+++ project/project/ProjectBuild.scala
@@ -6,6 +6,6 @@ import sbt.Keys._
 object ProjectBuild extends Build {
   lazy val projectBuild = project.in(file(".")).settings(List(
     addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0"),
-    addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.3")
+    addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.2")
   ): _*)
 }
